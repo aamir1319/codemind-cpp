1e70b43ba3a6a1ff551c9f067b4573abd3285bd7
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-10-12 12:19:51 +0530

    #include<bits/stdc++.h>
    using namespace std;
    
    int main()
    {
        int n;
        cin>>n;
        int flag=1;
        for(int i=0;i<n;i++)
        {
            int a;
            cin>>a;
            if (a%2==1){
                flag=0;
                break;
            }
        }
        if (flag)cout<<"True";
        else cout<<"False";
    }

---
+++ Even_Array.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int flag=1;
+    for(int i=0;i<n;i++)
+    {
+        int a;
+        cin>>a;
+        if (a%2==1){
+            flag=0;
+            break;
+        }
+    }
+    if (flag)cout<<"True";
+    else cout<<"False";
+}
